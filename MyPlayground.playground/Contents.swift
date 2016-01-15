51314c41ac231351e58e9facb9dae7b90342ff66
mamian <mamian521@gmail.com>
2016-01-15 10:25:20 +0800

    add MyPlayground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var isMyHouseOnFire:Bool=false
+var annotherBool=true
+
+if isMyHouseOnFire {
+    print("Someone get me some water!")
+    
+}else{
+    print("Someone get some fire for my house!")
+}
+
+var result=true==true
+result = true == false
+result = false == false
+
+var accountTotal = 300.33
+var newCallOfDutyGame = 59.99
+if accountTotal >= newCallOfDutyGame{
+    print("I just purchased the game!")
+    
+}else{
+    print("I dont have enough money")
+}
