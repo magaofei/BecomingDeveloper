3b2103ac1a9ca34df00d9d667e95f50e44802d07
mamian <mamian521@gmail.com>
2016-01-16 12:04:55 +0800

    学习了书上前三章

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -24,3 +24,18 @@ if accountTotal >= newCallOfDutyGame{
 }else{
     print("I dont have enough money")
 }
+
+var myString:String = ""
+var myOtherString:String?    //修饰符?将其制定为可选的
+var yetAnotherVariable:Float! //修饰符!指定为隐式拆封的
+
+
+// MARK - <变量声明shuxing，>
+
+for var  count=0;count<50;count=count+1{
+    //Do this ,50 times.
+}
+
+/* var myMemoryHog: SomeHugeObject ? = SomeHugeObject{}
+   myMemoryHog = nil   
+*/
