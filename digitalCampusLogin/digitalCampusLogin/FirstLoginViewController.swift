43a57cd8b47d137aeb75ca4353e4f699ec39af76
mamian <mamian521@gmail.com>
2016-01-22 23:29:12 +0800

    看教程把NSURLSession 的请求搞好了，明天做JSON解析

--- digitalCampusLogin/digitalCampusLogin/FirstLoginViewController.swift
+++ digitalCampusLogin/digitalCampusLogin/FirstLoginViewController.swift
@@ -11,12 +11,35 @@ import UIKit
 class FirstLoginViewController: UIViewController {
 
     @IBAction func LoginPressLogin(sender: UIButton) {
+        
     }
     
-    
-    
-    
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let urlString = "http://123.56.64.138/system/login.do?jobnumber=lanling&password=123456"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlString)!
+        
+        session.dataTaskWithURL(url) { (data: NSData?, response:  //加了个response:   NSData?前加了个data:
+            NSURLResponse?, error: NSError?) -> Void in    //加了个error: 在NSError前
+            
+            
+            if let responseData = data {
+                do{
+                    let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)
+                    
+                    print(json)
+                }catch{
+                    print("Could not Serialize")
+                }
+                
+            
+            }
+            
+        }.resume()
+        
+    }
+   
     
     
     
@@ -31,16 +54,19 @@ class FirstLoginViewController: UIViewController {
 
     
     @IBAction func exitToHome(sender: UIStoryboardSegue) {
+
         //不需要代码， 这是跳转回来的方法
-    }
+
     
-    
-    
+}
+}
+
+
+
     
     
     
       
     
     
-    }
 
