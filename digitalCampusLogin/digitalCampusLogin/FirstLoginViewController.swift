ddf12a7569fb5dd8e39e5d89eb208ad777f35713
mamian <mamian521@gmail.com>
2016-01-22 23:34:58 +0800

    给刚刚的NSURL加些注释，希望对理解有帮助

--- digitalCampusLogin/digitalCampusLogin/FirstLoginViewController.swift
+++ digitalCampusLogin/digitalCampusLogin/FirstLoginViewController.swift
@@ -15,17 +15,19 @@ class FirstLoginViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        let urlString = "http://123.56.64.138/system/login.do?jobnumber=lanling&password=123456"
-        let session = NSURLSession.sharedSession()
-        let url = NSURL(string: urlString)!
+        super.viewDidLoad()     //系统自带
+        
+        
+        let urlString = "http://123.56.64.138/system/login.do?jobnumber=lanling&password=123456"  //后台网址
+        let session = NSURLSession.sharedSession()   //Session方法
+        let url = NSURL(string: urlString)!       //设置urlSrting
         
         session.dataTaskWithURL(url) { (data: NSData?, response:  //加了个response:   NSData?前加了个data:
             NSURLResponse?, error: NSError?) -> Void in    //加了个error: 在NSError前
+            //方法的具体设置
             
-            
-            if let responseData = data {
-                do{
+            if let responseData = data {   //必须加do try catch  否则报错
+                do{                         //下面代码看起来还有问题
                     let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)
                     
                     print(json)
