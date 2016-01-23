ab823e3e136c8ba072cec2c84afef578f882baa2
mamian <mamian521@gmail.com>
2016-01-23 18:55:28 +0800

    暂时找到了解决JSON返回UTF8的问题，下一步需要修改

--- digitalCampusLogin/digitalCampusLogin/FirstLoginViewController.swift
+++ digitalCampusLogin/digitalCampusLogin/FirstLoginViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class FirstLoginViewController: UIViewController {
 
     @IBAction func LoginPressLogin(sender: UIButton) {
@@ -18,7 +20,7 @@ class FirstLoginViewController: UIViewController {
         super.viewDidLoad()     //系统自带
         
         
-        let urlString = "http://swapi.co/api/people/1/"  //后台网址
+        let urlString = "http://123.56.64.138/system/login.do?jobnumber=lanling&password=12345"  //后台网址
         let session = NSURLSession.sharedSession()   //Session方法
         let url = NSURL(string: urlString)!       //设置urlSrting
         
@@ -26,16 +28,27 @@ class FirstLoginViewController: UIViewController {
             NSURLResponse?, error: NSError?) -> Void in    //加了个error: 在NSError前
             //方法的具体设置
             
+            
+         
+            
+            
+            let myString = NSString(data:data!, encoding: NSUTF8StringEncoding) as! String
+            print(myString)   //  チェックのため、読み込んだデータをそのまま表示  http://ascii.jp/elem/000/001/044/1044645/index-2.html   解决了UTF的问题
+            //输出了结果， 而下面的没有输出
+            
+            
             if let responseData = data {   //必须加do try catch  否则报错
                 do{                         //下面代码看起来还有问题
-                    let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)   // AllowFragments指定顶级节点可以不是数组或字典 。 NSJSONSerialization 是IOS5后苹果提供的API，是JSON的解码框架
+                    let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.MutableContainers)   // AllowFragments指定顶级节点可以不是数组或字典 。 NSJSONSerialization 是IOS5后苹果提供的API，是JSON的解码框架
                     
                     if let dict = json as? Dictionary<String, AnyObject>{   //开始解析JSON   意思应该是 从字典当中寻找
                         //print("DID WE GET HERE: \(dict.debugDescription)")   // “\” 应当是一个分隔符，用来分割前面的字符串和后面的代码   成功
-                        if let name = dict["name"] as? String {   //如果里面有job_name的话就返回
+                        if let success = dict["success"] as? Bool,let message = dict["message"] as? String {   //如果里面有success的话就返回，要说明success 的值， 从LoginPostJSON.swift中调用
                             
-                            let LoginPost = LoginPostJSON(name: name)
-                                print(LoginPost.name)
+                            let LoginPost = LoginPostJSON(success: success,message: message)   //解析success
+                                print(LoginPost.success)
+                                print(LoginPost.message)
+                                print("成功解析")
                             
                         }
                     }
--- digitalCampusLogin/digitalCampusLogin/LoginPostJSON.swift
+++ digitalCampusLogin/digitalCampusLogin/LoginPostJSON.swift
@@ -9,27 +9,32 @@
 import Foundation
 class LoginPostJSON {
     private var _job_number: String!   //job_number ＝ lanling 用户名
-    private var _name: String!   //成功否   成功返回1 否则返回0
+    private var _success: Bool!   //success 是网络是否能连接上，并不代表能够登录
     private var _nick_name: String!
-    
+    private var _message: String!    //存放返回的消息
     
     var job_number: String{    //三个返回，暂时不明白
         return _job_number
     }
     
-    var name: String{
-        return _name
+    var success: Bool{
+        return _success
     }
     
     var nick_name:String{
         return  _nick_name
     }
     
+    var message:String{
+        return _message
+    }
     
     
     
     
-    init (name: String){  //应该为创建
-        _name = name
+    
+    init (success: Bool, message: String){  //应该为创建  ，把内部的值转换
+        _success = success
+        _message = message
     }
 }
