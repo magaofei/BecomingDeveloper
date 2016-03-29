d30e2018a1a4180e76a77153d62d72b03f6efa3f
mamian <mamian521@gmail.com>
2016-03-29 22:59:30 +0800

    增加了Java课堂练习1

---
+++ Java/src/LessonAssignment_1ReadAndParseInt.java
@@ -0,0 +1,15 @@
+/**
+ * Created by MAMIAN on 16/3/26.
+ */
+import java.util.Scanner;            //从命令行读入九个数放进int 数组里
+public class LessonAssignment_1ReadAndParseInt {
+public static void main(String[]args){
+    int stb[]= new int [8];
+    System.out.print("请输入九个数");
+    for (int i = 0; i <9 ; i++) {    //运行九次  循环输入   多于九次会溢出
+        Scanner input=new Scanner(System.in);
+        stb[i]=Integer.parseInt(input.next());   //字符串转换Int
+    }
+
+}
+}
