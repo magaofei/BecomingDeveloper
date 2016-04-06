46169094c69abb286030fc1c48f914d300ccf96f
mamian <mamian521@gmail.com>
2016-04-06 18:08:03 +0800

    类方法举例
    静态方法，在前面需加static ，表示类的共有行为

---
+++ Java/src/LessonAssignment/Converter.java
@@ -0,0 +1,11 @@
+
+public class Converter { //类方法举例
+
+	/**
+	 * @param args
+	 */
+public static int centigradeToFahrenheit(int cent){   //实现转换功能并将其放在Converter中
+	return (cent*9/5+32);
+}
+
+}
---
+++ Java/src/LessonAssignment/ConverterTester.java
@@ -0,0 +1,15 @@
+
+public class ConverterTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cent= 40;
+		int fah = Converter.centigradeToFahrenheit(40);
+		System.out.print("摄氏"+cent+"度相当于华氏"+fah+"度.");
+
+	}
+
+}
