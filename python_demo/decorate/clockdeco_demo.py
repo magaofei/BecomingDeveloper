9efccce2866bb6b4d617f2ced89ba9dd963baa17
Mark <mamian521@gmail.com>
2018-02-03 11:51:00 +0800

    add log decorator

--- python_demo/decorate/clockdeco_demo.py
+++ python_demo/decorate/clockdeco_demo.py
@@ -1,24 +1,44 @@
+
+import logging
+
 import time
 # from clockdeco import clock
 from clockdeco2 import clock
 import functools
 
+def log(func):
+    """
+    给func装饰
+
+    """
+    logger = logging.getLogger(__name__)
+    FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+    logging.basicConfig(format=FORMAT)
+    logger.setLevel(logging.DEBUG)
+
+    def logged(*arg, **kwargs):
+
+        logger.info(func.__name__)
+        result = func(*arg, **kwargs)
+        return result
+    return logged
+
+
+@log
 @clock
 def snooze(seconds):
-	time.sleep(seconds)
+    time.sleep(seconds)
 
 @clock
 def factorial(n):
 	return 1 if n < 2 else n * factorial(n - 1)
 
-
-
-
 if __name__ == '__main__':
-	print('*' * 40, 'Calling snooze(.123)')
-	snooze(.123)
-	print('*' * 40, 'Calling factorial(6)')
-	print('6 != ', factorial(600))
+
+    print('*' * 40, 'Calling snooze(.123)')
+    snooze(.123)
+    print('*' * 40, 'Calling factorial(6)')
+    print('6 != ', factorial(6))
 
 
 """
