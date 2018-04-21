4777713d0229ce3d39e9096c56491cd02aea9259
Mark <mamian521@gmail.com>
2018-04-21 10:18:26 +0800

    update singleton

--- python_demo/design_patten/singleton.py
+++ python_demo/design_patten/singleton.py
@@ -1,3 +1,10 @@
+__doc__ = """
+单例
+
+线程之间安全
+进程之间安全
+多进程下开多线程，同进程下安全
+"""
 import concurrent.futures
 class Singleton(object):
     _instances = {}
@@ -7,30 +14,58 @@ class Singleton(object):
         return cls._instances[cls]
 
 
-def singleton(class_):
+def singleton(cls):
     instances = {}
     def getinstance(*args, **kwargs):
-        if class_ not in instances:
-            instances[class_] = class_(*args, **kwargs)
-        return instances[class_]
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
     return getinstance
+
+try:
+    # This is jython-specific
+    from synchronize import make_synchronized
+except ImportError:
+    # This should work across different python implementations
+    def make_synchronized(func):
+        import threading
+        func.__lock__ = threading.Lock()
+
+        def synced_func(*args, **kws):
+            with func.__lock__:
+                return func(*args, **kws)
+
+        return synced_func
 
 
 @singleton
 class Test(object):
-    def __init__(self):
-        print(id(self))
+    def __init__(self, i):
+        pass
+        # print(i)
+        # print(id(self))
 
 
-def test_singleton():
-    for i in range(10):
-        t = Test()
+def test_singleton(i):
+    print(id(Test(i)))
 
+def open_threading():
+    print("threading")
+    with concurrent.futures.ThreadPoolExecutor(10) as future:
+        tests = [future.submit(test_singleton, i) for i in range(10)]
+        
 if __name__ == '__main__':
 
-    with concurrent.futures.ThreadPoolExecutor(10) as future:
-        f = future.submit(test_singleton)
-        print(f.result())
+    
+    print("process")
+    with concurrent.futures.ProcessPoolExecutor(10) as exector:
+        tests_process = [exector.submit(open_threading) for i in range(10)]
+        # for future in concurrent.futures.as_completed(tests):
+
+            
+            
+
+        
 
         # future.as_complate()
         
--- python_demo/generator/iter_number.py
+++ python_demo/generator/iter_number.py
@@ -13,22 +13,36 @@ class IterNumber(object):
         return self
     
     def __next__(self):
+        
         r = int(self.num / self.index % 10)
         if r <= 0:
             raise StopIteration
         self.index *= 10
         return r
 
+def y_number(number):
+    index = 1
+    r = 1
+    while r > 0:
+        r = int(number / index % 10)
+        index *= 10
+        yield r
+    
 
 
 if __name__ == '__main__':
-    i = IterNumber(123)
-    r = next(i)
-    print(r)
-    r = next(i)
-    print(r)
-    r = next(i)
-    print(r)
+    # i = IterNumber(123)
+    # for a in i:
+    #     print(a)
     # r = next(i)
     # print(r)
+    # r = next(i)
+    # print(r)
+    # r = next(i)
+    # print(r)
+    # r = next(i)
+    # print(r)
+    r = y_number(123)
+    for i in r:
+        print(i)
 
